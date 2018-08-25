efaf7297f77752350d19e81bd1de3f3e0e7a3ed5
nodox <snatera09@gmail.com>
2018-08-25 19:12:17 -0400

    initial complete port

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import './layout.css'
-
 const Layout = ({ children, data }) => (
   <StaticQuery
     query={graphql`
@@ -27,14 +25,7 @@ const Layout = ({ children, data }) => (
         >
           <html lang="en" />
         </Helmet>
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div>
           {children}
         </div>
       </>
